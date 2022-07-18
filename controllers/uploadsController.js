72671d133697f98fc0268787bdd1e6cb5268439f
nazar2419 <nazarx095@gmail.com>
2022-07-18 22:32:31 +0300

    Complete File Upload

---
+++ controllers/uploadsController.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const {StatusCodes} = require('http-status-codes');
+
+const uploadProductImage = async(req, res) => {
+  const productImage = req.files.image;
+
+  const imagePath = path.join(__dirname, '../public/uploads/'+`${productImage.name}`);
+  await productImage.mv(imagePath);
+  return res.status(StatusCodes.OK).json({image:{src:`${productImage.name}`}})
+};
+
+
+module.exports = {
+  uploadProductImage,
+}
