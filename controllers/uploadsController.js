058affd6ed608aaa156f61fc516198f4fb9e55f5
nazar2419 <nazarx095@gmail.com>
2022-07-19 20:49:38 +0300

    Additional Checks

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -3,11 +3,31 @@ const path = require('path');
 const {StatusCodes} = require('http-status-codes');
 
 const uploadProductImage = async(req, res) => {
+  //check if file exists
+  //check format
+  //check size
+  console.log(req.files);
+  if(!req.files){
+    throw new CustomError.BadRequestError('No file Upload');
+  }
+
   const productImage = req.files.image;
+
+  if(!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+  const maxSize = 1024 * 1024; // 1mb
+
+  if(!productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller 1KB');
+  }
 
   const imagePath = path.join(__dirname, '../public/uploads/'+`${productImage.name}`);
   await productImage.mv(imagePath);
-  return res.status(StatusCodes.OK).json({image:{src:`${productImage.name}`}})
+  return res
+    .status(StatusCodes.OK)
+    .json({image:{src:`/uploads/${productImage.name}`}})
 };
 
 
